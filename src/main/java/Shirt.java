969ad6dca72e84c87f6ce51a0358c57b8f4ace4e
Ellen Spertus <ellen.spertus@gmail.com>
2024-02-06 17:22:36 -0800

    Eliminate unneeded method

--- src/main/java/Shirt.java
+++ src/main/java/Shirt.java
@@ -21,20 +21,12 @@ public class Shirt implements Comparable<Shirt> {
         return String.format("%s shirt (size %d)", brand, size);
     }
 
-    private static ArrayList<Shirt> copyIntoArrayList(List<Shirt> oldList) {
-        ArrayList<Shirt> newList = new ArrayList<>(oldList.size());
-        for (Shirt shirt : oldList) {
-            newList.add(shirt);
-        }
-        return newList;
-    }
-
     public static void main(String[] args) {
         List<Shirt> shirts = List.of(
                 new Shirt(10,  "Adidas"),
                 new Shirt(6, "Hanes")
         );
-        List<Shirt> mutableShirts = copyIntoArrayList(shirts);
+        List<Shirt> mutableShirts = new ArrayList<>(shirts);
         System.out.println(mutableShirts);
         Collections.sort(mutableShirts);
         System.out.println(mutableShirts);
